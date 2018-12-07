2f26f9b4b97332b0a718a2dcbb500dd9d1cce469
yuanziluoye <fake>
2018-12-07 18:08:32 +0800

    add auth authentication

--- staticserver.go
+++ staticserver.go
@@ -7,95 +7,254 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
-	"regexp"
-	"strconv"
+	"io/ioutil"
+	"os/exec"
+	"strings"
 	"time"
+	"crypto/subtle"
+
+	"github.com/astaxie/beego/logs"
+	"gopkg.in/yaml.v2"
+	"github.com/gorilla/mux"
+	"github.com/urfave/negroni"
+	"golang.org/x/crypto/bcrypt"
 )
 
-var mux map[string]func(http.ResponseWriter, *http.Request)
-
-type Myhandler struct{}
 type home struct {
 	Title string
 }
 
-const (
-	Template_Dir = "./view/"
-	Upload_Dir   = "./upload/"
-)
-
-func main() {
-	server := http.Server{
-		Addr:        ":9090",
-		Handler:     &Myhandler{},
-		ReadTimeout: 10 * time.Second,
-	}
-	mux = make(map[string]func(http.ResponseWriter, *http.Request))
-	mux["/"] = index
-	mux["/upload"] = upload
-	mux["/file"] = StaticServer
-	server.ListenAndServe()
+type AppLogger struct {
+	Daily      bool   `yaml:"Daily"`
+	MaxDays    int64  `yaml:"MaxDays"`
+	Rotate     bool   `yaml:"Rotate"`
+	Level      int    `yaml:"Level"`
+	Perm       string `yaml:"Perm"`
+	RotatePerm string `yaml:"RotatePerm"`
+	LogPath    string `yaml:"LogPath"`
 }
 
-func (*Myhandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if h, ok := mux[r.URL.String()]; ok {
-		h(w, r)
-		return
-	}
-	if ok, _ := regexp.MatchString("/css/", r.URL.String()); ok {
-		http.StripPrefix("/css/", http.FileServer(http.Dir("./css/"))).ServeHTTP(w, r)
-	} else {
-		http.StripPrefix("/", http.FileServer(http.Dir("./upload/"))).ServeHTTP(w, r)
+type AppConfig struct {
+	Logger      AppLogger
+	ListenPort  string   `yaml:"listenPort"`
+	TemplateDir string   `yaml:"templateDir"`
+	StaticDir   string   `yaml:"staticDir"`
+	UploadDir   string   `yaml:"uploadDir"`
+	FileExtType []string `yaml:"fileExtType"`
+	User        string   `yaml:"user"`
+	Password    string   `yaml:"password"`
+}
+
+var logger = logs.NewLogger(10000)
+var appConfig = AppConfig{}
+var templateDir string
+var uploadDir string
+var staticDir string
+
+func init() {
+
+	// read config
+	currentPath := getCurrPath()
+	configFile := currentPath + "/config.yaml"
+	readData, err := ioutil.ReadFile(configFile)
+	if err != nil {
+		logger.Error("[config] read config file failed, %v", err)
+		os.Exit(0)
 	}
 
+	err = yaml.Unmarshal(readData, &appConfig)
+	if err != nil {
+		logger.Error("[config] parse config.yaml failed, %v", err)
+		os.Exit(0)
+	}
+
+	loggerConfig := appConfig.Logger
+	logPath := currentPath + "/" + loggerConfig.LogPath
+	if err := os.MkdirAll(filepath.Dir(logPath), 0755); err != nil {
+		logger.Error("[config] create log dir failed, %v", err)
+		os.Exit(0)
+	}
+
+	templateDir = filepath.FromSlash(currentPath + "/" + appConfig.TemplateDir)
+	staticDir = filepath.FromSlash(currentPath + "/" + appConfig.StaticDir)
+
+	uploadDir = filepath.Clean(appConfig.UploadDir)
+	if !filepath.IsAbs(uploadDir) {
+		uploadDir, _ = filepath.Abs(uploadDir)
+	}
+	if uploadDir[len(uploadDir)-1] != '/' {
+		uploadDir += "/"
+	}
+	uploadDir = filepath.FromSlash(uploadDir)
+
+	// logger config
+	loggerConfigJson := `{"filename":"%v", "daily": %v, "maxDays": %v, "rotate": %v, "level": %v, "perm":"%v", "rotateperm":"%v"}`
+	loggerJsonConfig := fmt.Sprintf(loggerConfigJson, logPath, loggerConfig.Daily, loggerConfig.MaxDays,
+		loggerConfig.Rotate, loggerConfig.Level, loggerConfig.Perm, loggerConfig.RotatePerm)
+
+	logger.SetLogger("file", loggerJsonConfig)
+	logger.EnableFuncCallDepth(true)
+	logger.SetLogFuncCallDepth(2)
+	logger.SetLogger("console", fmt.Sprintf(`{"level": %d}`, logs.LevelInformational))
+
+	logger.Info("[init] load config file: %s", configFile)
+	logger.Info("[init] use log path: %s", logPath)
+	logger.Info("[init] template path: %s", templateDir)
+	logger.Info("[init] static path: %s", staticDir)
+	logger.Info("[init] upload path: %s", uploadDir)
+}
+
+func main() {
+
+	topRouter := mux.NewRouter().StrictSlash(true)
+
+	topRouter.HandleFunc("/", index)
+
+	topRouter.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir(staticDir))))
+
+	topRouter.HandleFunc("/upload", upload)
+
+	topRouter.PathPrefix("/file").Handler(http.HandlerFunc(file))
+
+	topRouter.NotFoundHandler = http.HandlerFunc(notFound)
+
+	username := appConfig.User
+	password := appConfig.Password
+
+	n := negroni.New()
+	n.Use(negroni.HandlerFunc(func(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+		if BasicAuth(w, r, username, password, "Provide user name and password") {
+			/* Call the next handler iff Basic-Auth succeeded */
+			logger.Info("ping, %s, %s", r.RequestURI, time.Now().Format("2006-01-02 15:04:05.999999999"))
+
+			next(w, r)
+		}
+	}))
+
+	n.UseHandler(topRouter)
+
+	http.ListenAndServe(":"+appConfig.ListenPort, n)
+}
+
+func notFound(w http.ResponseWriter, r *http.Request) {
+
+	http.StripPrefix("/", http.FileServer(http.Dir(uploadDir))).ServeHTTP(w, r)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+
+	if r.URL.Path != "/" {
+		http.Error(w, "Not found", 404)
+		return
+	}
+
+	title := home{Title: "上传页"}
+
+	templateFile := templateDir + "index.html"
+	if _, err := os.Stat(templateFile); os.IsNotExist(err) {
+		logger.Error("[Template] template not exist, %v", err)
+		return
+	}
+
+	t, _ := template.ParseFiles(templateFile)
+	t.Execute(w, title)
 }
 
 func upload(w http.ResponseWriter, r *http.Request) {
 
 	if r.Method == "GET" {
-		t, _ := template.ParseFiles(Template_Dir + "file.html")
-		t.Execute(w, "上传文件")
-	} else {
-		r.ParseMultipartForm(32 << 20)
-		file, handler, err := r.FormFile("uploadfile")
-		if err != nil {
-			fmt.Fprintf(w, "%v", "上传错误")
+
+		templateFile := templateDir + "file.html"
+		if _, err := os.Stat(templateFile); os.IsNotExist(err) {
+			logger.Error("[Template] template not exist, %v", err)
 			return
 		}
-		fileext := filepath.Ext(handler.Filename)
-		if check(fileext) == false {
+
+		t, _ := template.ParseFiles(templateFile)
+		title := home{Title: "上传文件"}
+		t.Execute(w, title)
+
+	} else {
+		r.ParseMultipartForm(32 << 20)
+
+		file, handler, err := r.FormFile("uploadfile")
+		if file == nil {
+			fmt.Fprintf(w, "%v", "上传失败,文件为空")
+			logger.Error("[Upload] upload failed, %v", err)
+			return
+		}
+
+		if err != nil {
+			fmt.Fprintf(w, "%v", "上传失败")
+			logger.Error("[Upload] upload failed, %v", err)
+			return
+		}
+
+		filename := handler.Filename
+		fileext := filepath.Ext(filename)
+		if !stringInSlice(fileext, appConfig.FileExtType) {
 			fmt.Fprintf(w, "%v", "不允许的上传类型")
 			return
 		}
-		filename := strconv.FormatInt(time.Now().Unix(), 10) + fileext
-		f, _ := os.OpenFile(Upload_Dir+filename, os.O_CREATE|os.O_WRONLY, 0660)
+
+		f, _ := os.OpenFile(uploadDir+filename, os.O_CREATE|os.O_WRONLY, 0660)
 		_, err = io.Copy(f, file)
 		if err != nil {
 			fmt.Fprintf(w, "%v", "上传失败")
+			logger.Error("%v", err)
 			return
 		}
-		filedir, _ := filepath.Abs(Upload_Dir + filename)
-		fmt.Fprintf(w, "%v", filename+"上传完成,服务器地址:"+filedir)
+
+		w.Header().Set("Content-Type", "text/html; charset=UTF-8")
+		fileLink := `<br/><a href="/file" target="_blank">查看文件</a><br/><a href="/" target="_blank">返回首页</a>`;
+		fmt.Fprintf(w, "%s", filename+"上传完成"+fileLink)
 	}
 }
 
-func index(w http.ResponseWriter, r *http.Request) {
-	title := home{Title: "首页"}
-	t, _ := template.ParseFiles(Template_Dir + "index.html")
-	t.Execute(w, title)
+func file(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Cache-Control", "no-store, must-revalidate")
+	w.Header().Set("Expires", "0")
+	http.StripPrefix("/file", http.FileServer(http.Dir(uploadDir))).ServeHTTP(w, r)
 }
 
-func StaticServer(w http.ResponseWriter, r *http.Request) {
-	http.StripPrefix("/file", http.FileServer(http.Dir("./upload/"))).ServeHTTP(w, r)
-}
-
-func check(name string) bool {
-	ext := []string{".exe", ".js", ".png"}
-
-	for _, v := range ext {
-		if v == name {
-			return false
+func stringInSlice(a string, list []string) bool {
+	for _, b := range list {
+		if b == a {
+			return true
 		}
 	}
+	return false
+}
+
+func getCurrPath() string {
+	file, _ := exec.LookPath(os.Args[0])
+	path, _ := filepath.Abs(file)
+	index := strings.LastIndex(path, string(os.PathSeparator))
+	ret := path[:index]
+	return ret
+}
+
+func BasicAuth(w http.ResponseWriter, r *http.Request, username, password, realm string) bool {
+
+	user, pass, ok := r.BasicAuth()
+
+	if !ok || subtle.ConstantTimeCompare([]byte(user), []byte(username)) != 1 || !CheckPasswordHash(pass, password) {
+		w.Header().Set("WWW-Authenticate", `Basic realm="`+realm+`"`)
+		w.WriteHeader(401)
+		w.Write([]byte("Unauthorised.\n"))
+		return false
+	}
+
 	return true
+}
+
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 12)
+	//bytes, err := bcrypt.GenerateFromPassword([]byte(password), 10)
+	return string(bytes), err
 }
